9a979a0808e99782e934ffe87cfbe2600bd4a7e6
MuhammadHammadTahir <mhammadt04@gmail.com>
2025-03-08 23:51:17 +0500

    Authentication and registration process for Customer and Artist is completed

---
+++ Login.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Login.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login</title>
+    <link rel="icon" type="image/png" href="logo_new.png">
+    <link rel="stylesheet" type="text/css" href="../StyleSheet.css" />
+</head>
+<body>
+    <img accesskey="" src="logo_new.png" alt="logo" id="logo" />
+
+    <form id="form2" runat="server">
+        
+        <div id="message_box" runat="server"></div>
+        
+        <h2>Login</h2>
+
+        <div class="login-container">
+            <div runat="server">
+                <label for="person_id">CNIC:</label>
+                <input id="person_id" type="text" placeholder="e.g., 12345-1234567-1" class="form-control" runat="server" required="required" pattern="\d{5}-\d{7}-\d{1}" />
+            </div>
+
+            <div runat="server">
+                <label for="password">Password:</label>
+                <input id="password" type="password" class="form-control" runat="server" required="required" />
+            </div>
+        </div>
+
+        <div class="submit-container" runat="server">
+            <input id="login_btn" type="submit" value="Login" runat="server" />
+        </div>
+
+        <div class="sign-up">
+            <p>Don't have an account? <a href="Registration.aspx">Register Here</a></p>
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.vb
@@ -0,0 +1,60 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Security.Policy
+Imports System.ServiceModel.Channels
+
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Dim Session_Cnic As String
+    Dim Session_Password As String
+    Dim Session_Role As String
+    Dim Authenticated As Boolean = False
+
+    Dim is_error As Boolean = False
+    Private Sub Login(sender As Object, e As EventArgs) Handles login_btn.ServerClick
+        Dim constr As String
+        constr = "Data Source= localhost; Initial Catalog= ARTVAULT; User ID=Hammad; Password= Hammad"
+        Dim con As New SqlConnection
+        con.ConnectionString = constr
+        Dim cmd As New SqlCommand
+        cmd.Connection = con
+        cmd.CommandText = "SELECT PERSON_ID, ROLE, PASSWORD FROM PERSON WHERE PERSON_ID = '"
+        cmd.CommandText += person_id.Value + "' AND PASSWORD = '" + password.Value + "'"
+        Dim dr As SqlDataReader
+        Try
+            con.Open()
+            dr = cmd.ExecuteReader
+            If dr.Read() Then
+                message_box.Style.Add("opacity", "1")
+                message_box.InnerText = "Login Successful"
+                Authenticated = True
+                Session_Cnic = dr("PERSON_ID")
+                Session_Password = dr("PASSWORD")
+                Session_Role = dr("ROLE")
+                Session("Cnic") = Session_Cnic
+                Session("Password") = Session_Password
+                Session("Role") = Session_Role
+                Session("Authenticated") = Authenticated
+            Else
+                message_box.Style.Add("opacity", "1")
+                message_box.InnerText = "CNIC or Password is Incorrect"
+                is_error = True
+            End If
+        Catch ex As Exception
+            message_box.Style.Add("opacity", "1")
+            message_box.InnerText = ex.Message
+            is_error = True
+        Finally
+
+            con.Close()
+            cmd.Dispose()
+            If is_error = False Then
+                Response.Redirect("Login.aspx")
+            End If
+        End Try
+
+    End Sub
+
+
+End Class
